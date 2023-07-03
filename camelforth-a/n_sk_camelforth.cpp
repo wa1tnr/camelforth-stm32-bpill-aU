f9e2b247af101e3564c85218be241a5a1214e50e
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-03 16:21:42 +0000

    latest taming of serial beast to 115200
      The machine performs acceptably well for the
      purpose of simple testing now.
    
    	modified:   n_sk_camelforth.cpp
    
    On branch stm32

--- camelforth-a/n_sk_camelforth.cpp
+++ camelforth-a/n_sk_camelforth.cpp
@@ -5,19 +5,8 @@
 extern "C" {
 #endif
 
-// void experiment_a_asm(unsigned int *rptr);
-
-// extern void this_here();
-
 void this_here();
 void interpreter(void);
-
-/*
-char getch(void);
-void putch(char c);
-int getquery(void);
-*/
-
 
 extern void this_here_now();
 
@@ -30,18 +19,19 @@ char getch(void) {
     }
 
     while (!waiting_ch) {
-      waiting_ch = Serial.available();
+      waiting_ch = Serial1.available();
       // this_here_now(); // loop
     }
 
     if (waiting_ch) {
-      char ch = Serial.read();
+      char ch = Serial1.read();
       return ch;
     }
 }
 
 void putch(char c) {
-    printf("%c", c);
+    Serial1.write(c);
+    // printf("%c", c);
 
     return ;  // doesn't have to do anything
 }
@@ -52,45 +42,35 @@ int getquery(void) {
     return(0 != 0);
 }
 
-
-
-
-
-
 #ifdef __cplusplus
 }
 #endif
 
-
-
 void trapped() {
-  // this_here_now();
-  // this_here_now();
+  this_here_now();
+  this_here_now();
 
   interpreter();
 
   while(-1);
 }
 
-void setup() {
-  Serial.begin(9600);
-  Serial.write(' ');
-  Serial.println("\n\n Camelforth in C");
-  pinMode(15, OUTPUT);
+#define THIS_LED_BDEF 31
 
-/*
+void setup() {
+  Serial1.begin(115200);
+  Serial1.write(' ');
+  Serial1.println("\n\n Camelforth in C\n\n");
+  Serial1.println("2023 Jul 3rd 14:19:21 UTC\n\n");
+  Serial1.println(" - for black pill 411CE on Serial1 A9 A10\n\n");
+  pinMode(THIS_LED_BDEF, OUTPUT);
+
   for (int count = 3; count > 0; count--) {
-    digitalWrite(15, 1);
+    digitalWrite(THIS_LED_BDEF, 0);
     delay(90);
-    digitalWrite(15, 0);
+    digitalWrite(THIS_LED_BDEF, 1);
     delay(700);
   }
-*/
-
-  // this_here_now();
-
-  // pinMode(LED_BUILTIN, OUTPUT);
-
   trapped();
 }
 
